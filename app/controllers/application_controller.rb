ce0f62b8ecbbafd28d7a44f6becbcb893d7afb81
Dr Nic Williams <drnicwilliams@gmail.com>
2020-09-26 10:10:42 +1000

    wip for drnic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-  require 'xero-ruby'
+  require "xero-ruby"
 
   helper_method :current_user
   before_action :xero_client
@@ -12,24 +12,34 @@ class ApplicationController < ActionController::Base
     @token_set = @xero_client.get_token_set_from_callback(params)
     # you can use `@xero_client.connections` to fetch info about which orgs
     # the user has authorized and the most recently connected tenant_id
-    current_user.token_set = @token_set if !@token_set["error"]
-    current_user.token_set['connections'] = @xero_client.connections
-    current_user.active_tenant_id = latest_connection(current_user.token_set['connections'])
-    current_user.save!
-    flash.notice = "Successfully received Xero Token Set"
+    if (error = @token_set["error"])
+      pp @token_set
+      flash.notice = "Failed to authenticate: #{error}"
+    else
+      current_user.token_set = @token_set unless @token_set["error"]
+      current_user.token_set["connections"] = @xero_client.connections
+      current_user.active_tenant_id = latest_connection(current_user.token_set["connections"])
+      current_user.save!
+      flash.notice = "Successfully received Xero Token Set"
+    end
   end
 
   def refresh_token
     @token_set = @xero_client.refresh_token_set(current_user.token_set)
-    current_user.token_set = @token_set if !@token_set["error"]
-    current_user.token_set['connections'] = @xero_client.connections
-    current_user.save!
-    flash.notice = "Successfully Refreshed Token"
+    if (error = @token_set["error"])
+      pp @token_set
+      flash.notice = "Failed to refresh token: #{error}"
+    else
+      current_user.token_set = @token_set unless @token_set["error"]
+      current_user.token_set["connections"] = @xero_client.connections
+      current_user.save!
+      flash.notice = "Successfully Refreshed Token"
+    end
     redirect_to root_url
   end
 
   def change_organisation
-    current_user.active_tenant_id = params['change_organisation']['active_tenant_id']
+    current_user.active_tenant_id = params["change_organisation"]["active_tenant_id"]
     current_user.save!
     flash.notice = "Current Tenant/Org updated <strong>#{current_user.active_tenant_id}</strong>"
     redirect_to root_url
@@ -37,8 +47,8 @@ class ApplicationController < ActionController::Base
 
   def disconnect
     remaining_connections = @xero_client.disconnect(params[:connection_id])
-    current_user.token_set['connections'] = remaining_connections
-    current_user.active_tenant_id = latest_connection(current_user.token_set['connections'])
+    current_user.token_set["connections"] = remaining_connections
+    current_user.active_tenant_id = latest_connection(current_user.token_set["connections"])
     current_user.save!
     flash.notice = "Removed <strong>#{current_user.active_tenant_id}</strong>"
     redirect_to root_url
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,20 @@
 module ApplicationHelper
-  require 'jwt'
+  require "jwt"
 
   def token_expired
-    token_expiry = Time.at(access_token['exp'])
+    token_expiry = Time.at(access_token["exp"])
     exp_text = time_ago_in_words(token_expiry)
     puts "token_expiry #{token_expiry}"
-    puts "Time.now #{Time.now}"    
-    token_expiry > Time.now ? "in #{exp_text}" : "#{exp_text} ago" 
+    puts "Time.now #{Time.now}"
+    token_expiry > Time.now ? "in #{exp_text}" : "#{exp_text} ago"
   end
 
   def id_token
-    JWT.decode(current_user.token_set['id_token'], nil, false)[0] if current_user && current_user.token_set
+    JWT.decode(current_user.token_set["id_token"], nil, false)[0] if current_user && current_user.token_set
   end
 
   def access_token
-    JWT.decode(current_user.token_set['access_token'], nil, false)[0] if current_user && current_user.token_set
+    JWT.decode(current_user.token_set["access_token"], nil, false)[0] if current_user && current_user.token_set
   end
 
   def current_user
@@ -23,10 +23,10 @@ module ApplicationHelper
 
   def xero_client
     creds = {
-      client_id: ENV['CLIENT_ID'],
-      client_secret: ENV['CLIENT_SECRET'],
-      redirect_uri: ENV['REDIRECT_URI'],
-      scopes: ENV['SCOPES']
+      client_id: ENV["CLIENT_ID"],
+      client_secret: ENV["CLIENT_SECRET"],
+      redirect_uri: ENV["REDIRECT_URI"],
+      scopes: ENV["SCOPES"]
     }
     @xero_client ||= XeroRuby::ApiClient.new(credentials: creds)
 
@@ -34,32 +34,30 @@ module ApplicationHelper
       @xero_client.set_token_set(current_user.token_set)
     end
 
-    return @xero_client
+    @xero_client
   end
 
   def has_token_set?
     unless current_user
       if current_user && current_user.token_set
         redirect_to authorization_url
-        return
+        nil
       else
-        redirect_to '/users/new'
-        return
+        redirect_to "/users/new"
+        nil
       end
     end
   end
 
   def authorization_url
-    @authorization_url ||= xero_client.authorization_url 
+    @authorization_url ||= xero_client.authorization_url
   end
 
   def latest_connection(connections)
     if connections.length
-      connections.sort { |a,b|
-        DateTime.parse(a['updatedDateUtc']) <=> DateTime.parse(b['updatedDateUtc'])
-      }.first['tenantId']
-    else
-      nil
+      connections.min { |a, b|
+        DateTime.parse(a["updatedDateUtc"]) <=> DateTime.parse(b["updatedDateUtc"])
+      }["tenantId"]
     end
   end
 end
